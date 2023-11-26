c8928331961ecd4859e8859042e57c35218c9a30
tg3000 <teograur@gmail.com>
2023-11-26 17:41:24 +0100

    Added Model

--- agent.py
+++ agent.py
@@ -1,2 +1,31 @@
-import numpy
-print(numpy.__version__)
+from collections import deque
+from model import Linear_QNet, QTrainer
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent():
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    # TODO
+    def get_state():
+        pass
+
+    def remember():
+        pass
+    
+    def train_long_memory():
+        pass
+
+    def train_short_memory():
+        pass
+
+    def get_action():
+        pass
